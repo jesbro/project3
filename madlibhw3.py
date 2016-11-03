a0c472840d87902984dc1d5c4f9f8be9ba8e4668
jesbro <jesbro@umich.edu>
2016-11-03 16:04:01 -0400

    added comments

--- madlibhw3.py
+++ madlibhw3.py
@@ -13,11 +13,12 @@
 import nltk
 import random
 from nltk.tokenize import sent_tokenize, word_tokenize
-#from nltk.book import *
-#from nltk import bigrams
 
+#get text2
 sense = nltk.corpus.gutenberg.words('austen-sense.txt')[:150]
 
+#taken from madlib_generatorP3.py
+#puts spaces where they should be
 def spaced(word):
 	if word in [",", ".", "?", "!", ":", ";"]:
 		return word
@@ -26,6 +27,7 @@ def spaced(word):
 
 print("START*******\n\n")
 
+#tags words with pos abbreviations
 pos = nltk.pos_tag(sense)
 
 tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective", "PRP$":"a possessive pronoun"}
@@ -33,6 +35,8 @@ subprobs= {"NN":.15,"NNS":.1,"VB":.1,"JJ":.1, "PRP$":.1}
 
 final = []
 
+#taken from madlib_generatorP3.py
+#replaces words with inputted words
 for (word, tag) in pos:
 	if tag not in subprobs or random.random() > subprobs[tag]:
 		final.append(spaced(word))
@@ -40,6 +44,7 @@ for (word, tag) in pos:
 		new_word = input("Please enter %s:\n" % (tagmap[tag]))
 		final.append(spaced(new_word))
 
+#prints original text, followed by madlibbed text
 print ("".join(spaced(w) for w in sense))
 print ("\n", "".join(final))
 
