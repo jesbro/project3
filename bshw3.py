0359e696bd4b5c935b10c8a11ce2b6c70680f7ac
jesbro <jesbro@umich.edu>
2016-11-04 10:30:33 -0400

    updating question 2

--- bshw3.py
+++ bshw3.py
@@ -10,3 +10,31 @@
 
 # Deliverables
 # Make sure the new page is uploaded to your GitHub account.
+
+import requests
+from bs4 import BeautifulSoup
+from bs4 import NavigableString
+import re
+import urllib
+
+url = 'http://collemc.people.si.umich.edu/data/bshw3StarterFile.html'
+r = requests.get(url)
+f = open('bshw3.html', "w")
+
+soup = BeautifulSoup(r.text, "html.parser")
+#f.write(soup.encode("ascii", "ignore").decode("utf-8"))
+#for line in soup.find_all("a"):
+for line in soup.find_all("a"): #href=re.compile("student")):
+	if "student" in line.text:
+		#l = line.p.string
+		fixed = "***************"#line.text.replace("student", "AMAZING student")
+		line.replace_with(fixed)
+		print (line.text)
+	# print (line.text)
+	# print (line.text.encode("ascii", "ignore").decode("utf-8"))
+
+	# f.write(lines.text.replace("student", "AMAZING student").strip().\
+ #              encode("ascii", "ignore").decode("utf-8"))
+	# print (str(student).encode("ascii", "ignore").decode("utf-8"))
+
+f.close()
